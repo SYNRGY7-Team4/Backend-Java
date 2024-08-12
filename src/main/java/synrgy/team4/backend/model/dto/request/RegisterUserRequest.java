7b8166274b88caadc43c3ad05ec8a76e1ee01055
Recal Prasetyo <142847837+Stev1oL@users.noreply.github.com>
2024-08-13 04:13:10 +0700

    Update RegisterUserRequest.java

--- src/main/java/synrgy/team4/backend/model/dto/request/RegisterUserRequest.java
+++ src/main/java/synrgy/team4/backend/model/dto/request/RegisterUserRequest.java
@@ -1,9 +1,7 @@
 package synrgy.team4.backend.model.dto.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Data
