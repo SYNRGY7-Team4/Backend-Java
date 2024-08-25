17ea6397331d949817b2e3dc320f2676cd9801c2
Recal <iamrecal10@gmail.com>
2024-08-25 18:03:16 +0700

    added type debit and credit in transaction response

--- src/main/java/synrgy/team4/backend/model/dto/response/MutationResponse.java
+++ src/main/java/synrgy/team4/backend/model/dto/response/MutationResponse.java
@@ -22,11 +22,13 @@ public class MutationResponse {
     private String accountFrom;
     @JsonProperty("username_from")
     private String nameAccountFrom;
+    private String accountFromType;
 
     @JsonProperty("account_to")
     private String accountTo;
     @JsonProperty("username_to")
     private String nameAccountTo;
+    private String accountToType;
 
     private BigDecimal amount;
     private LocalDateTime datetime;
@@ -34,7 +36,4 @@ public class MutationResponse {
     private String status;
     private String description;
     private BigDecimal currentBalance;
-
-    public MutationResponse(UUID id, String accountNumber, String name, String accountNumber1, String name1, BigDecimal amount, LocalDateTime datetime, String type, String status, String description, AtomicReference<BigDecimal> currentBalance) {
-    }
 }
